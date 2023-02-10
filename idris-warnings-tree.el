d694463e704747fa85889ce545c916eecb5b39f6
Marek L <nospam.keram@gmail.com>
2023-02-10 13:54:50 +0000

    backup-remove-if-notes

--- idris-warnings-tree.el
+++ idris-warnings-tree.el
@@ -38,18 +38,16 @@
 (defvar idris-tree-printer 'idris-tree-default-printer)
 
 (defun idris-compiler-notes-list-show (notes)
-  (if (null notes)
-      nil ;; See https://github.com/idris-hackers/idris-mode/pull/148 TODO: revisit
-    (with-current-buffer (get-buffer-create idris-notes-buffer-name)
-      (idris-compiler-notes-mode)
-      (let ((buffer-read-only nil)
-            (root (idris-compiler-notes-to-tree notes)))
-        (erase-buffer)
-        (idris-tree-insert root "")
-        (insert "\n\n")
-        (message "Press q to close, return or mouse on error to navigate to source")
-        (goto-char (point-min))))
-    (display-buffer idris-notes-buffer-name)))
+  (with-current-buffer (get-buffer-create idris-notes-buffer-name)
+    (idris-compiler-notes-mode)
+    (let ((buffer-read-only nil)
+          (root (idris-compiler-notes-to-tree notes)))
+      (erase-buffer)
+      (idris-tree-insert root "")
+      (insert "\n\n")
+      (message "Press q to close, return or mouse on error to navigate to source")
+      (goto-char (point-min))))
+  (display-buffer idris-notes-buffer-name))
 
 (defun idris-tree-for-note (note)
   (let* ((buttonp (> (length (nth 0 note)) 0)) ;; if empty source location
